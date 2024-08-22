c06bdb18324dc1a1c8f1cac15756cbdbe6bb2b7e
adibahbab4108 <adibalvi476@gmail.com>
2024-08-23 00:10:39 +0600

    responsive header section for lg, md, sm devices done

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        extend: {
+            fontFamily:{
+                'manrope': ['Manrope', 'sans-serif'],
+            },
+            colors: {
+                
+            },
+        },
+
+    },
+    plugins: [],
+}
